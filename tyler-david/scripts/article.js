9822ee639d5f777735239688bf0861a5bba1a7c6
Tyler <Tconfalone@gmail.com>
2018-03-17 18:52:15 -0700

    trying to pull

--- tyler-david/scripts/article.js
+++ tyler-david/scripts/article.js
@@ -11,7 +11,7 @@ function Article (rawDataObj) {
   this.title = rawDataObj.title,
   this.category = rawDataObj.category,
   this.author = rawDataObj.author,
-  this.authorUrl = rawDataOnj.aurthorUrl,
+  this.authorUrl = rawDataObj.aurthorUrl,
   this.publishedOn = rawDataObj.publishedOn,
   this.body = rawDataObj.body
 }
@@ -22,7 +22,8 @@ Article.prototype.toHtml = function() {
 
   let $newArticle = $('article.template').clone();
   // /TODO: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
-  $(newArticle.removeClass(‘template’);
+  $newArticle.removeClass('template')
+
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
@@ -61,8 +62,6 @@ articles.forEach(function(articles) {
 // for (let i = 0; i < rawData.length; i++) {
 //   articles.push(new Article(rawData[i]));
 // }
-
 // for (let i = 0; i < articles.length; i++) {
   // un-comment when toHTML in good enough shape to avoid terrible browser hang
 $('#articles').append(articles[i].toHtml());
-}
