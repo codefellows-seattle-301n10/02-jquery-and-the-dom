c6b75e0a9ab8dc00bc3427af2a3c0545994d61c7
tamarushin <tamarushin@gmail.com>
2018-03-19 18:06:36 -0700

    forEach methods, push articles to constructor

--- nicholas-tama/scripts/article.js
+++ nicholas-tama/scripts/article.js
@@ -27,7 +27,7 @@ Article.prototype.toHtml = function() {
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  /* TODONE: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
       1. author name,
       2. author url,
@@ -36,6 +36,7 @@ Article.prototype.toHtml = function() {
       5. publication date. */
   $newArticle.find('.byline time').text(this.publishedOn) //Make sure to double check '.byline time' OR 'time'
   $newArticle.find('h1').text(this.title);
+  $newArticle.find('a').text(this.author);
   $newArticle.find('.byline a').attr('href', this.authorUrl);
   $newArticle.find('section.article-body').html(this.body);
 
@@ -52,11 +53,12 @@ rawData.sort(function(a,b) {
 
 // TODO: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
 
-for(let i = 0; i < articles.length; i++) {
+rawData.forEach(function(rawDataObj) {
+  articles.push(new Article(rawData));
+});
+
+articles.forEach(function(articles) {
   // un-comment when toHTML in good enough shape to avoid terrible browser hang
-  // $('#articles').append(articles[i].toHtml());
-}
+  $('#articles').append(articles[i].toHtml());
+})
